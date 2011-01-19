366552419f715a622300016ebe67d8ae090836e9
Michael Schubert <pair+mschubert@pivotallabs.com>
2011-01-19 18:17:35 -0500

    switch to rdoc documentation, move rake task into tasks file that can be included in rakefile

--- lib/chef_cap/tasks.rb
+++ lib/chef_cap/tasks.rb
@@ -1,13 +1,17 @@
+require 'rake'
+
 namespace :chef_cap do
   desc "Install chefcap JSON and cookbooks for the first time"
   task :install => :environment do
     require "fileutils"
+    templates_path = File.join(File.dirname(__FILE__), "..", "generators", "chef_cap", "templates", "chef")
+
+    FileUtils.cp_r File.join(templates_path, "..", "Capfile"), File.join(Rails.root, "Capfile")
 
     if File.exist?(Rails.root + "chef/node.json") || File.directory?(Rails.root + "chef/cookbooks")
-      puts "Already initialized?"
+      puts "Already initialized chef_cap?"
     else
       FileUtils.mkdir_p(Rails.root + "chef")
-      templates_path = File.join(File.dirname(__FILE__), "..", "lib", "generators", "chef_cap", "templates", "chef")
       FileUtils.cp_r Dir.glob(templates_path, "*")), (Rails.root + "chef")
     end
   end
